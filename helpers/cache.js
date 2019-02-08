23bda251925224e71e331ac22372594fad7434d4
yaroslav-codefresh <yaroslav@codefresh.io>
2019-02-08 12:13:50 +0200

    cache management

---
+++ helpers/cache.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const fs = require('fs');
+const request = require('requestretry');
+const CFError = require('cf-errors');
+const moment = require('moment');
+
+const defaults = require('../lib/defaults');
+const { handleErrors } = require('./error');
+
+async function _downloadSpec(url) {
+    try {
+        const response = await request({ url });
+        handleErrors(response);
+        return JSON.parse(response.body);
+    } catch (e) {
+        throw new CFError({ message: `Sdk: Could not load openapi.json from url: ${url}` });
+    }
+}
+
+/**
+ * loads spec either from filesystem cache or from url:
+ * 1) cache lifetime - 1 day
+ * 2) use forceRefresh in order to refresh cache anyway
+ * 3) use disableCache in order not to write/read cache at all
+ * */
+const loadOpenApiSpec = async ({ specUrl = null, disableCache = false, forceRefresh = false } = {}) => {
+    const url = specUrl || defaults.SPEC_URL;
+    if (disableCache) {
+        return _downloadSpec(url);
+    }
+
+    const currentDate = moment();
+    const cacheDir = path.join(defaults.CODEFRESH_PATH, 'openapi-cache');
+    const cacheFileName = `openapi-${currentDate.format('YYYY-MM-DD')}.json`;
+    const cachePath = path.join(cacheDir, cacheFileName);
+
+    const cacheExists = fs.existsSync(cachePath);
+
+    if (cacheExists && !forceRefresh) {
+        return JSON.parse(fs.readFileSync(cachePath, 'utf8'));
+    }
+    if (!fs.existsSync(defaults.CODEFRESH_PATH)) {
+        fs.mkdirSync(defaults.CODEFRESH_PATH);
+    }
+    if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir);
+    }
+
+    const spec = await _downloadSpec(url);
+    fs.writeFileSync(cachePath, JSON.stringify(spec, null, '\t'));
+    return spec;
+};
+
+/* eslint-disable */
+module.exports = {
+    loadOpenApiSpec,
+};
---
+++ helpers/error.js
@@ -0,0 +1,51 @@
+const CFError = require('cf-errors');
+const _ = require('lodash');
+
+function _makeResponseError(response) {
+    return new CFError({
+        statusCode: response.statusCode,
+        message: JSON.stringify(response.body || response),
+    });
+}
+
+function handleErrors(response) {
+    const { statusCode } = response;
+
+    // if for some reason request was not properly redirected (when "Location" header is lost, not usual case)
+    if (statusCode >= 300 && statusCode < 400) {
+        throw new CFError({
+            cause: _makeResponseError(response),
+            message: 'Error: Request was not properly redirected',
+        });
+    }
+    if (statusCode === 401) {
+        throw new CFError({
+            cause: _makeResponseError(response),
+            message: 'Error: Please create or update your authentication context',
+        });
+    }
+    if (statusCode === 403) {
+        throw new CFError({
+            cause: _makeResponseError(response),
+            message: 'Error: You do not have permissions to perform this action',
+        });
+    }
+
+    // other status codes
+    if (statusCode >= 400 && statusCode < 600) {
+        if (_.get(response, 'body.message')) {
+            if (_.get(response, 'body.error')) {
+                throw new CFError(`message: ${response.body.message}\nerror: ${response.body.error}`);
+            } else {
+                throw new CFError(response.body.message);
+            }
+        } else {
+            throw _makeResponseError(response);
+        }
+    }
+}
+
+/* eslint-disable */
+module.exports = {
+    handleErrors,
+};
