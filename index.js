9c49e416c61adf160c5b07ab530174e892b3339d
yaroslav-codefresh <yaroslav@codefresh.io>
2019-05-04 18:29:06 +0300

    update analyzer + add validator

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Sdk = require('./lib/Sdk');
 const Config = require('./lib/Config');
+const utils = require('./lib/utils');
 
 
 module.exports = {
     Codefresh: Sdk,
     Config,
+    utils,
 };
